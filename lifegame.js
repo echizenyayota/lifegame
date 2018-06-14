09b0f351cbad7a5279939f68e6a0b5dbe5416c82
echizenyayota <virtualmisso30@gmail.com>
2018-06-14 18:54:41 +0900

    プロトタイプはオブジェクトの__proto__を参考にする

--- lifegame.js
+++ lifegame.js
@@ -1,5 +1,15 @@
 "use strict";
 
+// プロトタイプはオブジェクトの__proto__を参考にする
+
+// プロトタイプがないオブジェクトを生成する
 var state = Object.create(null);
 var view = Object.create(null);
 var controls = Object.create(null);
+
+console.log(state);
+
+// プロトタイプがあるオブジェクト生成する
+var model = {};
+
+console.log(model);
